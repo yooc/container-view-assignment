eeb561cba20baca074783bc8f59803a77d0b612d
Amanda Rawls <jennifer.amanda.rawls@monsanto.com>
2018-03-14 06:15:42 -0500

    set up puppy view vc and favorites vc and container view

--- RateMyPuppy/RateMyPuppy/FavoriteViewController.swift
+++ RateMyPuppy/RateMyPuppy/FavoriteViewController.swift
@@ -2,6 +2,8 @@ import UIKit
 
 class FavoriteViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
--- RateMyPuppy/RateMyPuppy/PuppyViewController.swift
+++ RateMyPuppy/RateMyPuppy/PuppyViewController.swift
@@ -2,6 +2,8 @@ import UIKit
 
 class PuppyViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
