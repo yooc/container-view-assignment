79366a1ded4bb3013d0d22e7b7826d0135ca2603
Amanda Rawls <jennifer.amanda.rawls@monsanto.com>
2018-03-14 06:17:51 -0500

    added ibOutlet for puppy text

--- RateMyPuppy/RateMyPuppy/FavoriteViewController.swift
+++ RateMyPuppy/RateMyPuppy/FavoriteViewController.swift
@@ -3,6 +3,8 @@ import UIKit
 class FavoriteViewController: UIViewController {
 
     @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var favoriteText: UILabel!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
