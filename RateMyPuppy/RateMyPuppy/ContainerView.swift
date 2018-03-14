065967aae92fd18e7cabe4a62feb8d27b0fb4e55
Amanda Rawls <jennifer.amanda.rawls@monsanto.com>
2018-03-14 17:49:44 -0500

    Recovery!

---
+++ RateMyPuppy/RateMyPuppy/ContainerView.swift
@@ -0,0 +1,21 @@
+//
+//  ContainerView.swift
+//  RateMyPuppy
+//
+//  Created by RAWLS, JENNIFER AMANDA [AG/1000] on 3/14/18.
+//  Copyright © 2018 RAWLS, JENNIFER AMANDA [AG/1000]. All rights reserved.
+//
+
+import UIKit
+
+class ContainerView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
