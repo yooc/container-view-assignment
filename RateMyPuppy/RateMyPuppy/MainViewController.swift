2628e019bc3ab92047b4aafdff1c14eec49c5049
Amanda Rawls <jennifer.amanda.rawls@monsanto.com>
2018-03-14 06:23:21 -0500

    added an outlet for collection view and instantiated the model

--- RateMyPuppy/RateMyPuppy/MainViewController.swift
+++ RateMyPuppy/RateMyPuppy/MainViewController.swift
@@ -2,9 +2,12 @@ import UIKit
 
 class MainViewController: UIViewController {
 
+    @IBOutlet weak var puppyDetails: UIView!
+    
+    let model: RateMyPuppyModel = RateMyPuppyModel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
 }
 
