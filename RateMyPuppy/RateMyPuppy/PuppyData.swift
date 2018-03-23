f67107b898fa02df7e695c40204b44afda367453
Courtney Yoo <c.yoo@wustl.edu>
2018-03-22 21:23:38 -0500

    Starting

--- RateMyPuppy/RateMyPuppy/PuppyData.swift
+++ RateMyPuppy/RateMyPuppy/PuppyData.swift
@@ -39,18 +39,18 @@ class PuppyData {
     
     
     //MARK: - default data
-    let puppy1 = PuppyObject(name: "Puppy 1", image: "puppy1.jpg", description: "Puppy 1 description")
-    let puppy2 = PuppyObject(name: "Puppy 2", image: "puppy2.jpg", description: "Puppy 2 description")
-    let puppy3 = PuppyObject(name: "Puppy 3", image: "puppy3.jpg", description: "Puppy 3 description")
-    let puppy4 = PuppyObject(name: "Puppy 4", image: "puppy4.jpg", description: "Puppy 4 description")
-    let puppy5 = PuppyObject(name: "Puppy 5", image: "puppy5.jpg", description: "Puppy 5 description")
-    let puppy6 = PuppyObject(name: "Puppy 6", image: "puppy6.jpg", description: "Puppy 6 description")
+    let puppy1 = PuppyObject(name: "Pupper", image: "puppy1.jpg", description: "Very basic. Your generic pupper.")
+    let puppy2 = PuppyObject(name: "Fox-ish Corgi", image: "puppy2.jpg", description: "Cute in the kind of ugly way because is so stubby.")
+    let puppy3 = PuppyObject(name: "Pensive Puppy", image: "puppy3.jpg", description: "A little fluff ball with a lot of thoughts")
+    let puppy4 = PuppyObject(name: "Puppy 4", image: "puppy4.jpg", description: "Gets his eyebrows done by a professional.")
+    let puppy5 = PuppyObject(name: "Lamb", image: "puppy5.jpg", description: "Not actually a puppy at all...")
+    let puppy6 = PuppyObject(name: "Wide Eyed Doge", image: "puppy6.jpg", description: "Very curious and good at making you give him your left overs.")
     let puppy7 = PuppyObject(name: "Doge/Chihuahua Mix", image: "puppy7.jpg", description: "Loves long naps, barking at squirrels, eating your food. Hates STORMS.")
-    let puppy8 = PuppyObject(name: "Puppy 8", image: "puppy8.jpg", description: "Puppy 8 description")
+    let puppy8 = PuppyObject(name: "Puppy 4 again", image: "puppy8.jpg", description: "Can't fool me...")
     let puppy9 = PuppyObject(name: "Dachshund", image: "puppy9.jpg", description: "Sleeps all day...all night. Pretty much just a log that barks sometimes.")
-    let puppy10 = PuppyObject(name: "Puppy 10", image: "puppy10.jpg", description: "Puppy 10 description")
-    let puppy11 = PuppyObject(name: "Puppy 11", image: "puppy11.jpg", description: "Puppy 11 description")
-    let puppy12 = PuppyObject(name: "Puppy 12", image: "puppy12.jpg", description: "Puppy 12 description")
+    let puppy10 = PuppyObject(name: "Husky Buddies", image: "puppy10.jpg", description: "2 puppers are always better than 1")
+    let puppy11 = PuppyObject(name: "Sleepy Scrappy", image: "puppy11.jpg", description: "His name says it all...")
+    let puppy12 = PuppyObject(name: "Trash Panda", image: "puppy12.jpg", description: "Is not friends with Bradley Cooper.")
 }
 
 
